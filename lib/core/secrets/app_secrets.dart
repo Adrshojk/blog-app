14d35a2c9bd5a44294748a9c2cc947c5daa91af5
Adarsh j <67228948+Adrshojk@users.noreply.github.com>
2025-09-30 23:00:41 +0530

    Delete lib/core/secrets/app_secrets.dart
    deleted secret file

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const supabaseUrl = 'https://wyugjkiticoaajiodadm.supabase.co';
-  static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5dWdqa2l0aWNvYWFqaW9kYWRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyMDcyOTYsImV4cCI6MjA3NDc4MzI5Nn0.vjZH91vDS0MfpWTb4HbwjdmtzFPHjU-gHCZmkBoXHLg';
-}
